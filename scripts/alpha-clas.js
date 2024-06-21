38060c54fffb1e780b02f9ceb7f9c1395a0962c0
i-am-surovi <ssur.ovi03@gmail.com>
2024-06-21 12:17:50 +0600

    part 1 ends

--- scripts/alpha-clas.js
+++ scripts/alpha-clas.js
@@ -1,12 +1,33 @@
+// function play(){
+//     // step-1: hide the home screen by adding class hidden to the home section
+//     const homeSection = document.getElementById("home-screen");
+//     // add hidden in array like object of js
+//     homeSection.classList.add('hidden');
+
+//     // show the playground
+//     const playgroundSection = document.getElementById("play-ground");
+//     // remove hidden class from the classlist
+//     playgroundSection.classList.remove('hidden')
+
+// }
+
+
+function continueGame(){
+    // step-1: generate a random alphabet
+    const randomAlphabet = getARandomAlphabet();
+    console.log(randomAlphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById("current-alphabet");
+    currentAlphabetElement.innerText = randomAlphabet;
+
+    // set background color to keyboard key
+    setBackgroundColorById(randomAlphabet);
+}
+
+
 function play(){
-    // step-1: hide the home screen by adding class hidden to the home section
-    const homeSection = document.getElementById("home-screen");
-    // add hidden in array like object of js
-    homeSection.classList.add('hidden');
-
-    // show the playground
-    const playgroundSection = document.getElementById("play-ground");
-    // remove hidden class from the classlist
-    playgroundSection.classList.remove('hidden')
-
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -2,3 +2,29 @@ function hideElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
+function getARandomAlphabet(){
+    // get or create an alphabet array 
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetString.split("");
+    console.log(alphabets);
+
+    // get a random index between 0 to 25
+    const randomNumber = Math.random() * 25;
+    const randomIndex = Math.round(randomNumber);
+
+    const alphabet = alphabets[randomIndex];
+    return alphabet;
+}
